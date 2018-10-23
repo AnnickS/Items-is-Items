bd06285d4dc8c30770dc3b280f8cf04d8c06e2d1
Plum <pplluumm@users.noreply.github.com>
2018-10-23 09:57:35 -0700

    JordanMVP Created a moving character
    Created a character that moves on right click. Still working on getting it tested.

---
+++ JordanMVP/JordanMVP/Assets/Scripts/Movement/MoveTowardMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardMouse : MoveTowardPosition
+{
+    public int mouseButton = 1;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(mouseButton) || Input.GetMouseButton(mouseButton))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            base.goToPosition(mousePosition);
+        }
+    }
+}
---
+++ JordanMVP/JordanMVP/Assets/Scripts/Movement/MoveTowardPosition.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardPosition : MonoBehaviour
+{
+    public float speed = 0.05f;
+    private Vector2 targetPosition;
+    public bool moving = false;
+
+    void Start()
+    {
+        if(targetPosition == null)
+        {
+            targetPosition = this.transform.position;
+        }
+    }
+
+    public void goToPosition(Vector2 newGoalPosition)
+    {
+        targetPosition = newGoalPosition;
+        moving = true;
+    }
+
+    public Vector2 getCurrentTargetPosition()
+    {
+        return targetPosition;
+    }
+
+    public bool isCurrentlyMoving()
+    {
+        return moving;
+    }
+
+    void FixedUpdate()
+    {
+        if (isAlreadyAtTarget())
+        {
+            moving = false;
+        }
+        else if (moving == true)
+        {
+            Debug.Log("Moving");
+            move();
+        }
+    }
+
+    private bool isAlreadyAtTarget()
+    {
+        Vector2 currentPosition = getCurrentPositionXY();
+        return targetPosition.Equals(currentPosition);
+    }
+
+    private void move()
+    {
+        Vector2 currentPosition = getCurrentPositionXY();
+        Vector2 distanceBetween = targetPosition - currentPosition;
+
+        if (distanceBetween.magnitude <= speed)
+        {
+            setPositionXY(targetPosition);
+        }
+        else
+        {
+            distanceBetween.Normalize();
+            distanceBetween.Scale(new Vector2(speed, speed));
+            Vector3 newPosition = currentPosition + distanceBetween;
+
+            setPositionXY(newPosition);
+        }
+    }
+
+    private Vector2 getCurrentPositionXY()
+    {
+        Vector2 currentPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        return currentPosition;
+    }
+
+    private void setPositionXY(Vector2 newPosition)
+    {
+        this.transform.position = new Vector3(newPosition.x, newPosition.y, this.transform.position.z);
+    }
+}
