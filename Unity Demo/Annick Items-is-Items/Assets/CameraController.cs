2045cea39c7ed22f4bf3031e7c4e90ee76ff0569
AnnickS <32581380+AnnickS@users.noreply.github.com>
2018-10-28 22:00:10 -0700

    PlayerMovement/Items
    Player can move across a screen, collides with objects. Items can populate the screen and be affected by the player upon collision.

---
+++ Unity Demo/Annick Items-is-Items/Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
---
+++ Unity Demo/Annick Items-is-Items/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D playerBody;
+    Vector2 playerMovement = new Vector2();
+
+    private void FixedUpdate()
+    {
+        playerMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        playerBody.AddForce(playerMovement*speed);
+    }
+
+    // Use this for initialization
+    void Start () {
+        playerBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
