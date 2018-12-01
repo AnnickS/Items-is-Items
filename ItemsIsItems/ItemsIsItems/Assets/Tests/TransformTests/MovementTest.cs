36a505645e208aea43dca98707e84971fcd3108a
Plum <pplluumm@users.noreply.github.com>
2018-12-01 09:46:03 -0800

    Imported the movement tests
    Imported Movement tests, so we have movement and rotation. And the tests are working

---
+++ ItemsIsItems/ItemsIsItems/Assets/Tests/TransformTests/MovementTest.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class MovementTest
+{
+    
+    [UnityTest]
+    public IEnumerator FrameCountIncrementTest()
+    {
+        int before = Time.frameCount;
+        yield return null;
+        int after = Time.frameCount;
+        Assert.AreNotEqual(before, after);
+    }
+
+    [UnityTest]
+    public IEnumerator InstantiateCharacterTest()
+    {
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Character"));
+        Assert.IsNotNull(character);
+        MonoBehaviour.Destroy(character);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator MovesAwayTest()
+    {
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Character"));
+        MoveTowardPosition moveTowardPosition = character.GetComponent<MoveTowardPosition>();
+        Vector3 expected = character.transform.position;
+
+        moveTowardPosition.moveToPosition(new Vector2(1, 1));
+
+        Time.timeScale = 100;
+        yield return new WaitForSeconds(1);
+        Vector3 actual = character.transform.position;
+
+        Assert.False(actual.Equals(expected));
+        MonoBehaviour.Destroy(character);
+    }
+
+    [UnityTest]
+    public IEnumerator GoToPositionTest()
+    {
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Character"));
+        character.transform.position = new Vector2(0, 0);
+        MoveTowardPosition moveTowardPosition = character.GetComponent<MoveTowardPosition>();
+        Vector2 targetPosition = new Vector2(1, 1);
+
+        moveTowardPosition.moveToPosition(targetPosition);
+
+        Time.timeScale = 100;
+        yield return new WaitForSeconds(10);
+        Vector3 actual = character.transform.position;
+        Vector3 expected = new Vector3(targetPosition.x, targetPosition.y, character.transform.position.z);
+
+        Assert.True(actual.Equals(expected));
+        MonoBehaviour.Destroy(character);
+    }
+
+}
