21fe3d7335a071c9ae8bef7260531455b3e95680
bunrun11 <bunnydolphins_8@yahoo.com>
2018-12-03 16:06:35 -0800

    added inventory

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Inventory/TrailOffsetInventory.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailOffsetInventory : Inventory
+{
+    public float speed = 10;
+    private Dictionary<int, Item> selectedItems = new Dictionary<int, Item>();
+
+    public float distance = 1;
+    public float segmentSize = 0.1f;
+    private List<Vector3> points = new List<Vector3>();
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        if(belongsTo == null)
+        {
+            return;
+        }
+
+        if (points.Count == 0 || Vector3.Distance(points[0], transform.position) >= segmentSize)
+        {
+            if (points.Count > items.Count * (distance / segmentSize) +1)
+            {
+                points.RemoveAt(points.Count-1);
+            }
+            points.Insert(0, transform.position);            
+        }
+
+        for(int i = 0; i < items.Count; i++){
+            if(points.Count <= (i+1) * Mathf.RoundToInt(distance / segmentSize))
+            {
+                break;
+            }
+            Item item = items[i];
+            item.GetComponent<MoveTowardPosition>().moveToPosition(points[(i+1)* Mathf.RoundToInt(distance / segmentSize)]);
+        }
+
+        /*
+        foreach(KeyValuePair<int, Item> pair in selectedItems)
+        {
+            MoveSelectedItem(pair.Key, pair.Value);
+        }*/
+    }
+
+    public override void Open()
+    {
+        return;
+    }
+
+    public override void Close()
+    {
+        return;
+    }
+
+
+    public override void AddItem(Item item)
+    {
+        base.AddItem(item);
+    }
+
+    public override void RemoveItem(Item item)
+    {
+        base.RemoveItem(item);
+    }
+
+
+    /*
+    private void OnFingerDown(Touch touch)
+    {
+        
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            switch (LayerMask.LayerToName(hit.collider.gameObject.layer))
+            {
+                case "Item":
+                    Item item = hit.transform.GetComponent<Item>();
+                    if (Contains(item))
+                    {
+                        RemoveItem(item);
+                        selectedItems.Add(touch.fingerId, item);
+                        InputManager.instance.OnTouchEnd(touch.fingerId, OnFingerUp);                        ;
+                        //hit.transform.GetComponent<Movement>().SetTargetPosition(touch);
+                        //item.GetComponent<Movement>().CancelMove();
+                    }
+                    break;
+            }
+        }
+        
+    }
+    */
+
+    /*
+    private void OnFingerUp(Touch touch)
+    {
+        //selectedItems.Remove(touch.fingerId);
+    }
+    */
+
+    /*
+    private void MoveSelectedItem(int touchID, Item item)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(InputManager.instance.GetTouch(touchID).Value.position);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, 10000f, 1 << LayerMask.NameToLayer("Ground"));
+        item.GetComponent<Movement>().SetTargetPosition(hit.point);
+    }*/
+
+
+}
---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Item/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveTowardPosition))]
+public class Item : MonoBehaviour
+{
+    public GameObject graphicalObj;
+    public bool isPickupable = true;
+
+    MoveTowardPosition movement;
+
+    void Start()
+    {
+        movement = GetComponent<MoveTowardPosition>();
+        
+        if(transform.Find("Mesh") != null)
+        {
+            graphicalObj = transform.Find("Mesh").gameObject;
+        }
+        else
+        {
+            graphicalObj = gameObject;
+        }
+    }
+
+    /*
+    public void OnCollisionEnter(Collision collision)
+    {
+        Item other = collision.gameObject.GetComponent<Item>();
+        if (other != null)
+        {
+            if (GetHashCode() > other.GetHashCode())
+            {
+                GameManager.gameManager.OnItemTouch(new ItemInteractionRequest(this, other));
+            }
+        }        
+    }
+    */
+
+}
