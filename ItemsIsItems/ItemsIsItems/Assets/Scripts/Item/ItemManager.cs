46f7824be0b525845374455d5529ae53164eaaf1
AnnickS <32581380+AnnickS@users.noreply.github.com>
2018-12-04 21:25:10 -0800

    Added Item Usage!
    Item has been created. Item manager has been created. Item can be dragged if in inventory. Item can be used on player! \o/

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Item/ItemManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+    public static ItemManager Manager;
+
+	// Use this for initialization
+	void Start () {
+        Manager = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UseItem(Collider2D useItem, Collider2D affectedItem)
+    {
+        string itemUse = useItem.GetComponent<Item>().Tags[0];
+        if (itemUse.Equals("RED"))
+        {
+            changeColor(affectedItem, Color.red);
+        } else if (itemUse.Equals("YELLOW"))
+        {
+            changeColor(affectedItem, Color.yellow);
+        }
+
+        Destroy(useItem.gameObject);
+    }
+
+    void changeColor(Collider2D affectedItem, Color color)
+    {
+        affectedItem.GetComponent<SpriteRenderer>().material.color = color;
+    }
+}
