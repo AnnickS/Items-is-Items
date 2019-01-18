70f21ba4b5bade885267e90ac5697e33d09a980d
Plum <pplluumm@users.noreply.github.com>
2019-01-18 11:12:53 -0800

    Add a bunch of effects
    Spawn Effect
    Multiple Effects

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Item/EffectMultipleEffects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectMultipleEffects : Effect
+{
+
+    public List<Effect> effects;
+
+    public new void actOn(GameObject item1, GameObject item2)
+    {
+        foreach(Effect effect in effects)
+        {
+            effect.actOn(item1, item2);
+        }
+    }
+}
---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Item/EffectSpawnItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSpawnItem : Effect {
+
+    public Item itemToSpawn;
+
+    public void actOn(GameObject item1, GameObject item2)
+    {
+        Item item = GameObject.Instantiate<Item>(itemToSpawn);
+        item.transform.position = item2.transform.position;
+    }
+}
