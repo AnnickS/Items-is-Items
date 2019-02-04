4c75b3ad34d105a24d81e26e329a8ff7f5049f14
Plum <pplluumm@users.noreply.github.com>
2019-02-04 12:35:15 -0800

    Added QuestState Cycle

--- ItemsIsItems/ItemsIsItems/Assets/Scripts/Quest/Quest.cs
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Quest/Quest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,10 +10,43 @@ public class Quest : MonoBehaviour
 
     public Dialog dialog;
     public string text = "Hi I'm flowey";
+    public List<QuestState> states = new List<QuestState>();
+    public QuestState currentState;
 
     void Start()
     {
         dialog.setText(text);
+
+        states.Add(new QuestStateAskFor());
+        states.Add(new QuestStateIdle());
+
+        goToNextState();
+    }
+
+    private void goToNextState()
+    {
+        int currentStateIndex = getCurrentStateIndex();
+
+        if (currentStateIndex >= states.Count)
+        {
+            return;
+        }
+        else
+        {
+            currentState = states[currentStateIndex + 1];
+        }
+    }
+
+    private int getCurrentStateIndex()
+    {
+        if (currentState == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return states.IndexOf(currentState);
+        }
     }
 
     public void OnMouseDown()
