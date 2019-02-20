4eba86fac6776ac1f565850fa8c5e87d89a3fbb4
AnnickS <32581380+AnnickS@users.noreply.github.com>
2019-02-19 18:32:53 -0800

    Changed Select Target
    NPC now selects a target based on descriptors and not strings.

--- ItemsIsItems/ItemsIsItems/Assets/Scripts/NPC/NPCTarget.cs
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/NPC/NPCTarget.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(DetectionRange))]
 public class NPCTarget : MonoBehaviour {
     [SerializeField]
-    private List<string> AttractedTo;
+    private List<Descriptor> AttractedTo;
     [SerializeField]
-    private List<string> ScaredOf;
+    private List<Descriptor> ScaredOf;
     [SerializeField]
-    private List<string> HostileTo;
+    private List<Descriptor> HostileTo;
     private DetectionRange Vision;
     public Vector2 Target;
     private Vector2 Base;
+    Descriptor smelly;
 
     // Use this for initialization
     void Start () {
         Base = Target = transform.position;
         Vision = gameObject.GetComponent<DetectionRange>();
+        smelly = Resources.Load<Descriptor>("Descriptors/Smelly");
     }
 
     //Gets a direction for the npc to go towards
@@ -38,7 +40,7 @@ public class NPCTarget : MonoBehaviour {
 
             //NOTE: Needs to change to accept Tags instead of Strings
             //Items that the npc is scared of has first priority
-            if (ScaredOf.Find(x => name.Contains(x)) != null)
+            if (ScaredOf.Find(x => current.HasDescriptor(x)))
             {
                 gameObject.GetComponent<NPC>().Wait = 100;
                 Vector2 ItemPosition = new Vector2(cTransform.position.x, cTransform.position.y);
@@ -49,12 +51,12 @@ public class NPCTarget : MonoBehaviour {
 
                 return ItemPosition;
             }//Items that the npc is hostile to has next priority
-            else if (HostileTo.Find(x => name.Contains(x)) != null)
+            else if (HostileTo.Find(x => current.HasDescriptor(x)))
             {
                 gameObject.GetComponent<NPC>().Wait = 100;
                 return new Vector2(cTransform.position.x, cTransform.position.y);
             }//Items that the npc is attracted to have last priority
-            else if (AttractedTo.Find(x => name.Contains(x)) != null)
+            else if (AttractedTo.Find(x => current.HasDescriptor(x)))
             {
                 gameObject.GetComponent<NPC>().Wait = 100;
                 return new Vector2(cTransform.position.x, cTransform.position.y);
@@ -64,31 +66,30 @@ public class NPCTarget : MonoBehaviour {
         for (int i = 0; i < Vision.WithinSmell.Count; i++)
         {
             current = Vision.WithinSmell[i];
-            cTransform = current.GetComponent<Transform>();
             string name = current.gameObject.name;
 
-            if (ScaredOf.Find(x => name.Contains(x)) != null)
+            if (ScaredOf.Find(x => current.HasDescriptor(x)) != null)
             {
                 //Items that the npc is scared of has first priority
-                if (name.Contains("Smellable"))
+                if (current.HasDescriptor(smelly))
                 {
                     gameObject.GetComponent<NPC>().Rotate = true;
                     return CurrentPosition;
                 }
             }//Items that the npc is hostile to has next priority
-            else if (HostileTo.Find(x => name.Contains(x)) != null)
+            else if (HostileTo.Find(x => current.HasDescriptor(x)) != null)
             {
                 //Items that the npc is scared of has first priority
-                if (name.Contains("Smellable"))
+                if (current.HasDescriptor(smelly))
                 {
                     gameObject.GetComponent<NPC>().Rotate = true;
                     return CurrentPosition;
                 }
             }//Items that the npc is attracted to have last priority
-            else if (AttractedTo.Find(x => name.Contains(x)) != null)
+            else if (AttractedTo.Find(x => current.HasDescriptor(x)) != null)
             {
                 //Items that the npc is scared of has first priority
-                if (name.Contains("Smellable"))
+                if (current.HasDescriptor(smelly))
                 {
                     gameObject.GetComponent<NPC>().Rotate = true;
                     return CurrentPosition;
