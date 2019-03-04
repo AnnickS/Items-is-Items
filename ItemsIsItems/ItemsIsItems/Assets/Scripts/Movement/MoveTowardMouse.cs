2c8902bfcd1706097faaa8442dbec954e6fd8e51
bunrun11 <bunnydolphins_8@yahoo.com>
2019-03-03 20:56:18 -0800

    Patched moving while dragging

--- ItemsIsItems/ItemsIsItems/Assets/Scripts/Movement/MoveTowardMouse.cs
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Movement/MoveTowardMouse.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class MoveTowardMouse : MoveTowardPosition
 {
     public int mouseButton = 1;
+    
+    private bool holding = false;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(mouseButton) || Input.GetMouseButton(mouseButton))
+        if (Input.GetMouseButtonDown(mouseButton))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            base.moveToPosition(mousePosition);
+            
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f);
+            if (hit && hit.transform.GetComponent<Item>() != null && hit.transform.GetComponent<Item>().inventory != false)
+            {
+                holding = true;
+            }
+            else
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                base.moveToPosition(mousePosition);
+            }
+            
+        }
+        else if (Input.GetMouseButton(mouseButton))
+        {
+            if (holding == false)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                base.moveToPosition(mousePosition);
+            }
+        }
+        else
+        {
+            holding = false;
         }
     }
 }
