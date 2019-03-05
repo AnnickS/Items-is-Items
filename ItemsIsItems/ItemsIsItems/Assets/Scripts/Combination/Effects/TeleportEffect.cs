0f66e0c0157f15e05efeef9c180f2ea10eaa801a
bunrun11 <bunnydolphins_8@yahoo.com>
2019-03-04 20:01:51 -0800

    Added TeleportEffect

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Combination/Effects/TeleportEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TeleportEffect : Effect
+{
+    public GameObject teleportPosition;
+
+    public override void Execute(Item sender, Item interactor)
+    {
+        interactor.gameObject.transform.position = teleportPosition.transform.position;
+    }
+
+    public override bool IsInitialized()
+    {
+        return teleportPosition != null;
+    }
+}
