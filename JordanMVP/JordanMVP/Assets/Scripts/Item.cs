048850d7817c72451b745ad8311f6d5b8597f03f
AnnickS <32581380+AnnickS@users.noreply.github.com>
2019-03-06 14:45:27 -0800

    Removed demo projects

--- JordanMVP/JordanMVP/Assets/Scripts/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(Collider2D))]
-public abstract class Item : Selectable{
-
-    public abstract void usedOn(Item itemUsed);
-}
