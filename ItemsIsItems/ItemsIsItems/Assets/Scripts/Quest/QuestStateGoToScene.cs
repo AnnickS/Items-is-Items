be13bb563ea16283117e44596e65b6f3c30d63fe
Plum <pplluumm@users.noreply.github.com>
2019-03-06 16:50:47 -0800

    Add VictoryScreen

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/Quest/QuestStateGoToScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LoadScene))]
+public class QuestStateGoToScene : QuestState
+{
+    public string SceneToLoad;
+
+    public override void Initialize(GameObject questGiver)
+    {
+        GetComponent<LoadScene>().LoadSceneByName(SceneToLoad);
+    }
+
+    public override bool IsDone()
+    {
+        return false; //Will go to a different scene instead of loading
+    }
+
+    public override void ShowStory()
+    {
+
+    }
+}
