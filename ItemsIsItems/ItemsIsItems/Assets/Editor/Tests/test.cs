2ea17e39466d3a41d1c32e917848e64c98aba225
Plum <pplluumm@users.noreply.github.com>
2019-05-08 16:53:59 -0700

    Tweaks
    Removed RosePlayer Combination
    Removed TestCombination Combination
    Removed EffectChangeColor Effect
    Renamed TeleportEffect to EffectTeleportToJail

---
+++ ItemsIsItems/ItemsIsItems/Assets/Editor/Tests/test.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class test
+{
+    [Test]
+    public void ExampleQuestIsNotNull()
+    {
+        GameObject quest = MonoBehaviour.Instantiate(Resources.Load<GameObject>("ExampleQuest"));
+        Assert.NotNull(quest);
+        MonoBehaviour.Destroy(quest);
+    }
+
+    [Test]
+    public void ExampleQuestQuestComponentIsNotNull()
+    {
+
+        GameObject quest = MonoBehaviour.Instantiate(Resources.Load<GameObject>("ExampleQuest"));
+        QuestGiver questComponent = quest.GetComponent<QuestGiver>();
+        Assert.NotNull(questComponent);
+        MonoBehaviour.Destroy(quest);
+    }
+
+    [Test]
+    public void ExampleQuestDialogIsNotNull()
+    {
+
+        GameObject quest = MonoBehaviour.Instantiate(Resources.Load<GameObject>("ExampleQuest"));
+        Dialog dialog = quest.GetComponentInChildren<Dialog>();
+        Assert.NotNull(dialog);
+        MonoBehaviour.Destroy(quest);
+    }
+
+    [Test]
+    public void tesdt()
+    {
+        GameObject questGiver = new GameObject();
+        GameObject dialog = new GameObject();
+        GameObject textMesh = new GameObject();
+
+        dialog.transform.parent = questGiver.transform;
+        textMesh.transform.parent = dialog.transform;
+
+        textMesh.AddComponent<TextMeshPro>();
+        dialog.AddComponent<DialogPopup>();
+        questGiver.AddComponent<QuestGiver>();
+
+        MonoBehaviour.Destroy(questGiver);
+    }
+
+}
