6afc52ad5fc5951703110374cacafa2462f8d569
AnnickS <32581380+AnnickS@users.noreply.github.com>
2019-05-15 15:11:02 -0700

    Revert "Updated Tests"
    This reverts commit b1bc77f00bffee04e6874d6c9c4075c7d2ae27d9.

--- ItemsIsItems/ItemsIsItems/Assets/Tests/TransformTests/MovementTest.cs
+++ ItemsIsItems/ItemsIsItems/Assets/Tests/TransformTests/MovementTest.cs
@@ -18,7 +18,7 @@ public class MovementTest
     [UnityTest]
     public IEnumerator InstantiateCharacterTest()
     {
-        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreFabs/Character"));
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Character"));
         Assert.IsNotNull(character);
         MonoBehaviour.Destroy(character);
         yield return null;
@@ -27,7 +27,7 @@ public class MovementTest
     [UnityTest]
     public IEnumerator MovesAwayTest()
     {
-        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreFabs/Character"));
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Character"));
         MoveTowardPosition moveTowardPosition = character.GetComponent<MoveTowardPosition>();
         Vector3 expected = character.transform.position;
 
@@ -37,8 +37,6 @@ public class MovementTest
         yield return new WaitForSeconds(1);
         Vector3 actual = character.transform.position;
 
-        Debug.Log(actual);
-
         Assert.False(actual.Equals(expected));
         MonoBehaviour.Destroy(character);
     }
@@ -46,7 +44,7 @@ public class MovementTest
     [UnityTest]
     public IEnumerator GoToPositionTest()
     {
-        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("PreFabs/Character"));
+        GameObject character = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Character"));
         character.transform.position = new Vector2(0, 0);
         MoveTowardPosition moveTowardPosition = character.GetComponent<MoveTowardPosition>();
         Vector2 targetPosition = new Vector2(1, 1);
@@ -54,9 +52,8 @@ public class MovementTest
         moveTowardPosition.moveToPosition(targetPosition);
 
         Time.timeScale = 100;
-        yield return new WaitForSeconds(0);
+        yield return new WaitForSeconds(10);
         Vector3 actual = character.transform.position;
-
         Vector3 expected = new Vector3(targetPosition.x, targetPosition.y, character.transform.position.z);
 
         Assert.True(actual.Equals(expected));
