f39757a1317173971aef7a60451828a3ca712d77
Plum <pplluumm@users.noreply.github.com>
2019-05-31 15:25:14 -0700

    Created Pause Menu
    Created Pause Menu with:
    Open the pause menu with ESC, this sets the timescale to 0
    Main Menu - sends you to the main menu scene
    Restart - reloads the current scene
    Resume - closes the pause menu and sets the pause scale to what it was before

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public static bool isGamePaused = false;
+    public GameObject pauseMenu;
+    public KeyCode pauseKey;
+    protected static float defaultGameSpeed = 1;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isGamePaused = true;
+        defaultGameSpeed = Time.timeScale;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        isGamePaused = false;
+        Time.timeScale = defaultGameSpeed;
+        pauseMenu.SetActive(false);
+    }
+}
