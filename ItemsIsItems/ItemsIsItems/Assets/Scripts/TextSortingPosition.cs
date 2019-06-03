3ec1ddcb0981aeaeff09bd9539c8969bfd048735
AnnickS <32581380+AnnickS@users.noreply.github.com>
2019-06-03 14:14:03 -0700

    FixedDialogue
    Boxes and Text no longer rotate or move positions

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/TextSortingPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSortingPosition : MonoBehaviour {
+    private Vector3 pos;
+    private Quaternion rot;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Renderer>().sortingLayerName = "UI";
+        pos = transform.position;
+        rot = transform.rotation;
+    }
+
+    private void Update()
+    {
+        transform.position = pos;
+        transform.rotation = rot;
+    }
+
+
+}
