e4d6b944be761d416c6e831ad026330542f3697b
AnnickS <32581380+AnnickS@users.noreply.github.com>
2019-06-03 00:42:58 -0700

    FixedTextSortingLayer

---
+++ ItemsIsItems/ItemsIsItems/Assets/Scripts/TextSortingLayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSortingLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingLayerName = "UI";
+}
+}
